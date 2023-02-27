48b63a0a26ce4714c29a825ade9d4ad1e12617d6
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 <chip.gentoo@gmail.com>
2023-02-27 17:45:14 +0300

    add FOR for print array family

--- Home_154/Program.cs
+++ Home_154/Program.cs
@@ -44,3 +44,16 @@ int[,] arrayGrade = {
     {9, 4},
 };
 
+void prntArray(string[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+prntArray(arrayFamily);
